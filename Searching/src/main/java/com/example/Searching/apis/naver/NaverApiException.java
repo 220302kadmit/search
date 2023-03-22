9073a08083b98280bbcab2088cf7abcc8fba43c4
220302kadmit <128613574+220302kadmit@users.noreply.github.com>
2023-03-22 23:35:54 +0900

    Delete Searching directory

--- Searching/src/main/java/com/example/Searching/apis/naver/NaverApiException.java
+++
@@ -1,9 +0,0 @@
-package com.example.Searching.apis.naver;
-
-
-public class NaverApiException extends RuntimeException {
-
-    public NaverApiException(String message) {
-        super(message);
-    }
-}
